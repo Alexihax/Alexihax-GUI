2405057319f3ac7a94e9358d31c9ebf4f89e36a1
Alexihax <Alexihax@gmail.com>
2026-06-11 18:09:51 +0200

    Update Example.lua

--- Example.lua
+++ Example.lua
@@ -1,1 +1,7 @@
+local Library = loadstring(game:HttpGet(
+"https://raw.githubusercontent.com/YOURNAME/AstraUI/main/Library.lua"
+))()
 
+local Window = Library:CreateWindow({
+    Name = "Astra Hub"
+})
