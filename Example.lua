fb81780a72d66412d05b2ece9f2ac72fc29c2e9c
Alexihax <Alexihax@gmail.com>
2026-06-11 18:13:46 +0200

    Update Example.lua

--- Example.lua
+++ Example.lua
@@ -1,7 +1,11 @@
 local Library = loadstring(game:HttpGet(
-"https://raw.githubusercontent.com/YOURNAME/AstraUI/main/Library.lua"
+"https://raw.githubusercontent.com/Alexihax/Alexihax-GUI/main/Library.lua"
 ))()
 
 local Window = Library:CreateWindow({
     Name = "Astra Hub"
 })
+
+local Combat = Window:CreateTab("Combat")
+local Player = Window:CreateTab("Player")
+local Visuals = Window:CreateTab("Visuals")
