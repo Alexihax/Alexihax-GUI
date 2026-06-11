52a01e31b7802ad28caae3891e859591c6411cc2
Alexihax <Alexihax@gmail.com>
2026-06-11 18:13:26 +0200

    Add CreateTab method to Library window

--- Library.lua
+++ Library.lua
@@ -4,8 +4,20 @@ function Library:CreateWindow(options)
     local Window = {}
 
     Window.Name = options.Name or "Window"
+    Window.Tabs = {}
 
     print("Created window:", Window.Name)
+
+    function Window:CreateTab(name)
+        local Tab = {}
+
+        Tab.Name = name
+        table.insert(self.Tabs, Tab)
+
+        print("Created tab:", name)
+
+        return Tab
+    end
 
     return Window
 end
