7f6d4347ec75706c0fadb47a966f10921cefe5f8
Alexihax <Alexihax@gmail.com>
2026-06-11 19:17:41 +0200

    1

--- Example.lua
+++ Example.lua
@@ -1,13 +1,31 @@
 local Library = loadstring(game:HttpGet(
-"https://raw.githubusercontent.com/Alexihax/Alexihax-GUI/main/Library.lua?"..tick()
+    "https://raw.githubusercontent.com/Alexihax/Alexihax-GUI/main/Library.lua?" .. tick()
 ))()
 
 local Window = Library:CreateWindow({
     Name = "Alexihax Hub"
 })
 
+-- Combat tab
 local Combat = Window:CreateTab("Combat")
 
 Combat:CreateSection("General")
 Combat:CreateSection("Weapon")
 Combat:CreateSection("Actions")
+
+-- Player tab
+local Player = Window:CreateTab("Player")
+
+Player:CreateSection("Movement")
+Player:CreateSection("Character")
+
+-- Visuals tab
+local Visuals = Window:CreateTab("Visuals")
+
+Visuals:CreateSection("ESP")
+Visuals:CreateSection("World")
+
+-- Misc tab
+local Misc = Window:CreateTab("Misc")
+
+Misc:CreateSection("Utilities")
