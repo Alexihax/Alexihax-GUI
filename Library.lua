c096aa5e166ccb38efabbe0d049bafb72ebaa731
Alexihax <Alexihax@gmail.com>
2026-06-11 18:09:34 +0200

    Update Library.lua

--- Library.lua
+++ Library.lua
@@ -1,1 +1,13 @@
+local Library = {}
 
+function Library:CreateWindow(options)
+    local Window = {}
+
+    Window.Name = options.Name or "Window"
+
+    print("Created window:", Window.Name)
+
+    return Window
+end
+
+return Library
